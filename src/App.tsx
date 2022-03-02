9c1e8d8ec4399fb3be2452f33a58891329ba0a2b
Austin Walela <austinmasibo@gmail.com>
2022-03-02 22:07:58 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { useEffect, useState, useReducer, useCallback } from 'react'
+import { useEffect, useState } from 'react'
 import { usePagination } from 'react-use-pagination'
 import getPokemon from './api/services/getPokemon'
 import Layout from './components/Layout'
 import PokemonCard from './components/PokemonCard'
-import { IPokemonPreview } from './types'
 
-interface PaginationProps {
-  totalItems: number
-}
+import { RevolvingDot } from 'react-loader-spinner'
 
 export default function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false)
   const [pokemon, setPokemon] = useState<any>([])
   const [offset, setOffset] = useState<any>(0)
 
   const {
     currentPage,
     totalPages,
-    setPage,
     setNextPage,
     setPreviousPage,
   } = usePagination({
@@ -27,20 +24,24 @@ export default function App() {
   })
 
   useEffect(() => {
+    setLoading(true)
     getPokemon()
       .then((res) => {
         setCount(res.count)
         setPokemon(res.pokemon)
       })
-      .catch()
+      .then(() => setLoading(false))
+      .catch((err) => console.log(err))
   }, [])
 
   useEffect(() => {
+    setLoading(true)
     getPokemon(16, offset)
       .then((res) => {
         setPokemon(res.pokemon)
       })
-      .catch()
+      .then(() => setLoading(false))
+      .catch((err) => console.log(err))
   }, [offset])
 
   const setNext = () => {
@@ -54,44 +55,55 @@ export default function App() {
   }
 
   return (
-    <div>
-      <Layout>
-        <div className="w-full flex flex-col justify-center gap-4 flex-wrap mb-4">
+    <Layout>
+      <div className="w-full flex flex-col justify-center gap-4 flex-wrap mb-4">
+        {loading ? (
+          <div className="w-3/4  mx-auto flex justify-center py-12 items-center">
+            <RevolvingDot
+              radius={5}
+              width={120}
+              height={120}
+              color="rgb(57, 67, 202)"
+            />
+          </div>
+        ) : (
           <div className="w-full flex justify-center gap-4 flex-wrap mb-4">
-            {pokemon.map((p: any) => (
-              <div key={p.id}>
-                <PokemonCard
-                  id={p.id}
-                  name={p.name}
-                  types={p.types}
-                  imageURL={p.imageURL}
-                />
-              </div>
-            ))}
+            {pokemon.map((p: any) => {
+              return (
+                <div key={p.id}>
+                  <PokemonCard
+                    id={p.id}
+                    name={p.name}
+                    types={p.types}
+                    imageURL={p.imageURL}
+                  />
+                </div>
+              )
+            })}
           </div>
-          <div className="w-full mx-auto my-4 flex gap-3 justify-center items-center">
-            <button
-              className="btn-primary inline w-32"
-              onClick={() => setOffset(0)}
-            >
-              Home{' '}
-            </button>
-            <button
-              className="btn-primary"
-              onClick={setPrevious}
-              disabled={offset === 0}
-            >
-              Previous
-            </button>
-            <span className="text-gray-700 font-medium">
-              Current Page: {currentPage + 1} of {totalPages}
-            </span>
-            <button className="btn-primary" onClick={setNext}>
-              Next
-            </button>
-          </div>
+        )}
+        <div className="w-full mx-auto my-4 flex gap-3 justify-center items-center">
+          <button
+            className="btn-primary inline w-32"
+            onClick={() => setOffset(0)}
+          >
+            Home{' '}
+          </button>
+          <button
+            className="btn-primary"
+            onClick={setPrevious}
+            disabled={offset === 0}
+          >
+            Previous
+          </button>
+          <span className="text-gray-700 font-medium">
+            Current Page: {currentPage + 1} of {totalPages}
+          </span>
+          <button className="btn-primary" onClick={setNext}>
+            Next
+          </button>
         </div>
-      </Layout>
-    </div>
+      </div>
+    </Layout>
   )
 }
